836f43060f120bec54413708303703fb4b4678d1
Unknown <florent.baris@epitech.eu>
2018-03-01 20:41:17 +0100

    algo random

---
+++ hashcode2k18/hashcode2k18/Algo.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "SDR.cpp"
+#include <memory>
+#include <fstream> 
+#include <algorithm>
+#include <ctime> 
+
+void	algo(std::shared_ptr<SDR> infos)
+{
+	std::srand(unsigned(std::time(0)));
+	std::ofstream outfile("rendu.txt");
+
+	std::random_shuffle(infos->ridesList.begin(), infos->ridesList.end());
+	int i = 0;
+	for (auto it = infos->ridesList.begin(); it != infos->ridesList.end(); it++)
+	{
+		infos->cars.at(i++ % infos->cars.size)->rides.push_back(*it);
+	}
+	i = 0;
+	for (auto it = infos->cars.begin(); it != infos->cars.end(); it++)
+	{
+		outfile << i++;
+		for (auto jt = (*it)->rides.begin(); jt != (*it)->rides.end(); jt++)
+			outfile << " " << (*jt)->_id;
+	}
+	outfile.close();
+}
